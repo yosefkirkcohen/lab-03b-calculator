30e8758ca95ebca8774ed72384a1924a104a5abf
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-12 15:30:28 -0700

    addition works

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const addNumbers = (num1, num2) => {
+  return num1 + num2;
+}
+
+const firstNumber = document.getElementById('first-number');
+
+const secondNumber = document.getElementById('second-number');
+
+const addButton = document.getElementById('add-button');
+
+const answerSpace = document.getElementById('answer-space')
+
+
+addButton.addEventListener('click', () => {
+  const result = addNumbers(Number(firstNumber.value), Number(secondNumber.value))
+  answerSpace.textContent = result
+  
+})
+
