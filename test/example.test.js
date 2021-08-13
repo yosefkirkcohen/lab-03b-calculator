604cee28e38837df3d9a2f2e2005fe03cce79542
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-12 19:27:10 -0700

    working tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers } from "../utils.js";
+
 
 const test = QUnit.test;
 
@@ -11,6 +13,62 @@ test('time to test a function', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('adding test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = addNumbers(2, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('subtract test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtractNumbers(10, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('multiply test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 25;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiplyNumbers(5, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('division test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divideNumbers(25, 5);
 
     //Expect
     // Make assertions about what is expected versus the actual result
